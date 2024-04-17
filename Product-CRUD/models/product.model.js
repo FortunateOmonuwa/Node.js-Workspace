2abad0394d99ba45eda3263d8aff59ad0e781f96
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2024-04-17 12:50:32 +0100

    add product project

---
+++ Product-CRUD/models/product.model.js
@@ -0,0 +1,32 @@
+const { Timestamp } = require("mongodb");
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const ProductSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+exports.Product = mongoose.model("Product", ProductSchema);
